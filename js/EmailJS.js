91d6f1a24adf30f58e307d246eca818dd975da79
Bilawal Khan <131751844+EngrBilawalKhan@users.noreply.github.com>
2026-08-15 19:47:42 +0500

    Add files via upload

---
+++ js/EmailJS.js
@@ -0,0 +1,7 @@
+<script src="https://cdn.jsdelivr.net/cdn-js/emailjs-com@3/dist/email.min.js"></script>
+<script>
+    (function() {
+        // Replace with your actual EmailJS Public Key
+        emailjs.init("YOUR_PUBLIC_KEY");
+    })();
+</script>
